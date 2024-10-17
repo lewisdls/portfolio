937bc082b0024a1596731dbb491da71588848c08
Lewis Nomar <lewisdls21@hotmail.com>
2024-10-17 10:42:29 -0400

    Fixed url on Github icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
             <FaLinkedin className="text-3xl" />
           </a>
           <a
-            href="https://www.linkedin.com/in/lewisdls/"
+            href="https://www.github.com/lewisdls/"
             target="_blank"
             rel="noreferrer"
             className="hidden lg:block"
