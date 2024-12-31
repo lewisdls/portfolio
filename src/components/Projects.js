69128d3a44986e441590e200bd0dbfe5028a6f75
Lewis Nomar <lewisdls21@hotmail.com>
2024-12-31 09:36:03 -0400

    Adding new project!

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,6 +33,41 @@ const Projects = () => {
         <div className="w-full">
           <img
             className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
+            src="/store.png"
+            alt=""
+          />
+          <div className="flex flex-col gap-4 p-6 bg-[#181c4a] rounded-b-3xl w-full">
+            <h3 className="font-semibold text-2xl">Phoenix Store</h3>
+            <div className="flex flex-col lg:flex-row gap-2">
+              <div className="flex gap-2">
+                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
+                  Next.js
+                </p>
+                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
+                  TypeScript
+                </p>
+                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
+                  Tailwind
+                </p>
+              </div>
+              <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default self-start">
+                Sanity
+              </p>
+            </div>
+            <a
+              href="https://phoenix-store-gamma.vercel.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4">
+                Live demo <FiExternalLink />
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="w-full">
+          <img
+            className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
             src="/finance.png"
             alt=""
           />
