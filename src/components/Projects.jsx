4e7782b109a84689315c3be2367164915b3b0ab5
Lewis Nomar <lewisdls21@hotmail.com>
2025-04-27 19:43:09 -0400

    Enhancing Projects animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { FiExternalLink } from "react-icons/fi";
+import { projects } from "../utils/constants";
 
 const Projects = () => {
   return (
@@ -20,154 +21,51 @@ const Projects = () => {
           Each project is a unique piece of development
         </h2>
       </motion.div>
-      <motion.div
-        className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 text-left"
-        initial={{ opacity: 0, translateY: 100 }}
-        whileInView={{ opacity: 1, translateY: 0 }}
-        transition={{
-          duration: 0.8,
-          delay: 0.3,
-        }}
-      >
-        <div className="w-full">
-          <img
-            className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
-            src="/store.png"
-            alt=""
-          />
-          <div className="flex flex-col gap-4 p-6 bg-[#091447] rounded-b-3xl w-full">
-            <h3 className="font-semibold text-2xl">Phoenix Store</h3>
-            <div className="flex flex-col lg:flex-row gap-2">
-              <div className="flex gap-2 flex-wrap">
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Next.js
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  TypeScript
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Tailwind
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Sanity
-                </p>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 text-left">
+        {projects.map((project, index) => (
+          <motion.div
+            key={index}
+            className="w-full"
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.8,
+              delay: index * 0.1,
+            }}
+          >
+            <img
+              className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
+              src={project.image}
+              alt=""
+            />
+            <div className="flex flex-col gap-4 p-6 bg-[#091447] rounded-b-3xl w-full">
+              <h3 className="font-semibold text-2xl">{project.name}</h3>
+              <div className="flex flex-col lg:flex-row gap-2">
+                <div className="flex gap-2 flex-wrap">
+                  {project.tools.map((tool, index) => (
+                    <span
+                      key={index}
+                      className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default"
+                    >
+                      {tool}
+                    </span>
+                  ))}
+                </div>
               </div>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noreferrer"
+                className="w-fit"
+              >
+                <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4 cursor-pointer">
+                  Live demo <FiExternalLink />
+                </button>
+              </a>
             </div>
-            <a
-              href="https://phoenix-store-gamma.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-              className="w-fit"
-            >
-              <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4 cursor-pointer">
-                Live demo <FiExternalLink />
-              </button>
-            </a>
-          </div>
-        </div>
-        <div className="w-full">
-          <img
-            className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
-            src="/finance.png"
-            alt=""
-          />
-          <div className="flex flex-col gap-4 p-6 bg-[#091447] rounded-b-3xl w-full">
-            <h3 className="font-semibold text-2xl">DoSantos Finance App</h3>
-            <div className="flex flex-col lg:flex-row gap-2">
-              <div className="flex gap-2 flex-wrap">
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Next.js
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  TypeScript
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Tailwind
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  MongoDB
-                </p>
-              </div>
-            </div>
-            <a
-              href="https://dosantos-finance.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-              className="w-fit"
-            >
-              <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4 cursor-pointer">
-                Live demo <FiExternalLink />
-              </button>
-            </a>
-          </div>
-        </div>
-        <div className="w-full">
-          <img
-            className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
-            src="/salon-aura.jpeg"
-            alt=""
-          />
-          <div className="flex flex-col gap-4 p-6 bg-[#091447] rounded-b-3xl w-full">
-            <h3 className="font-semibold text-2xl">Salon Aura</h3>
-            <div className="flex flex-col lg:flex-row gap-2">
-              <div className="flex gap-2 flex-wrap">
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Next.js
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Tailwind
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  PostgreSQL
-                </p>
-              </div>
-            </div>
-            <a
-              href="https://salon-aura.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-              className="w-fit"
-            >
-              <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4 cursor-pointer">
-                Live demo <FiExternalLink />
-              </button>
-            </a>
-          </div>
-        </div>
-        <div className="w-full">
-          <img
-            className="w-full object-cover object-top rounded-t-3xl lg:h-[200px]"
-            src="/fitness.png"
-            alt=""
-          />
-          <div className="flex flex-col gap-4 p-6 bg-[#091447] rounded-b-3xl w-full">
-            <h3 className="font-semibold text-2xl">DoSantos Fitness Gym</h3>
-            <div className="flex flex-col lg:flex-row gap-2">
-              <div className="flex gap-2 flex-wrap">
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Next.js
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  Tailwind
-                </p>
-                <p className="px-2 py-1 border-blue-900 border-solid border-2 rounded-lg cursor-default">
-                  PostgreSQL
-                </p>
-              </div>
-            </div>
-            <a
-              href="https://fitness-rust-nine.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-              className="w-fit"
-            >
-              <button className="flex items-center gap-2 font-semibold text-md bg-white text-black w-max mt-4 px-2 py-1 rounded-xl border-b-blue-900 border-b-4 cursor-pointer">
-                Live demo <FiExternalLink />
-              </button>
-            </a>
-          </div>
-        </div>
-      </motion.div>
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 };
