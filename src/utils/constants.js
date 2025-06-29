a1b21d498382e8f5f558ab05836ae5594bedc1c6
Lewis Nomar <lewisdls21@hotmail.com>
2025-06-29 14:06:57 -0400

    Updating gym project's skills

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,26 @@
 export const skills = [
-    { name: "HTML", icon: "https://skillicons.dev/icons?i=html" },
-    { name: "CSS", icon: "https://skillicons.dev/icons?i=css" },
-    { name: "JavaScript", icon: "https://skillicons.dev/icons?i=js" },
-    { name: "TypeScript", icon: "https://skillicons.dev/icons?i=ts" },
-    { name: "React", icon: "https://skillicons.dev/icons?i=react" },
-    { name: "Node.js", icon: "https://skillicons.dev/icons?i=nodejs" },
-    { name: "Next.js", icon: "https://skillicons.dev/icons?i=nextjs" },
-    { name: "Tailwind", icon: "https://skillicons.dev/icons?i=tailwind" },
-    { name: "MongoDB", icon: "https://skillicons.dev/icons?i=mongodb" },
-    { name: "PostgreSQL", icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" },
-    { name: "Sanity", icon: "https://yt3.googleusercontent.com/NAUB0vdxKU7a0n2L863_S3cn35TLev08dKY7P8NqTZn4EH3hDXh-v1oPWPDgasSRUokqlAtg=s900-c-k-c0x00ffffff-no-rj" },
-    { name: "Strapi", icon: "https://media2.dev.to/dynamic/image/width=320,height=320,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Forganization%2Fprofile_image%2F763%2F988af53b-5d7e-435a-98eb-dd4aff5299d2.png" },
-  ];
+  { name: "HTML", icon: "https://skillicons.dev/icons?i=html" },
+  { name: "CSS", icon: "https://skillicons.dev/icons?i=css" },
+  { name: "JavaScript", icon: "https://skillicons.dev/icons?i=js" },
+  { name: "TypeScript", icon: "https://skillicons.dev/icons?i=ts" },
+  { name: "React", icon: "https://skillicons.dev/icons?i=react" },
+  { name: "Node.js", icon: "https://skillicons.dev/icons?i=nodejs" },
+  { name: "Next.js", icon: "https://skillicons.dev/icons?i=nextjs" },
+  { name: "Tailwind", icon: "https://skillicons.dev/icons?i=tailwind" },
+  { name: "MongoDB", icon: "https://skillicons.dev/icons?i=mongodb" },
+  {
+    name: "PostgreSQL",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
+  },
+  {
+    name: "Sanity",
+    icon: "https://yt3.googleusercontent.com/NAUB0vdxKU7a0n2L863_S3cn35TLev08dKY7P8NqTZn4EH3hDXh-v1oPWPDgasSRUokqlAtg=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Strapi",
+    icon: "https://media2.dev.to/dynamic/image/width=320,height=320,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Forganization%2Fprofile_image%2F763%2F988af53b-5d7e-435a-98eb-dd4aff5299d2.png",
+  },
+];
 
 export const projects = [
   {
@@ -35,7 +44,7 @@ export const projects = [
   {
     name: "Dosantos Fitness Gym",
     image: "/fitness.png",
-    tools: ["Next.js", "Tailwind", "PostgreSQL"],
+    tools: ["Next.js", "Tailwind"],
     link: "https://fitness-rust-nine.vercel.app",
   },
 ];
